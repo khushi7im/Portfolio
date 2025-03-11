3f0bff5f0e55c04a4dba6df07a89c64911a0da9e
khushi Vashisth <khushidhiman951@gmail.com>
2025-03-11 21:21:43 +0530

    firebase: realtimeDB

--- personal-portfolio/src/components/contact.jsx
+++ personal-portfolio/src/components/contact.jsx
@@ -1,7 +1,22 @@
 import React, { useState } from "react";
 import "./contact.css";
-import TopImg from "/home2.jpg";
-import { motion } from "framer-motion";
+import { getDatabase, ref, push } from "firebase/database";
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_KEY,
+  authDomain: import.meta.env.VITE_DOMAIN,
+  databaseURL: import.meta.env.VITE_URL,
+  projectId: import.meta.env.VITE_ID,
+  storageBucket: import.meta.env.VITE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_SENDERID,
+  appId: import.meta.env.VITE_APPID,
+  measurementId: import.meta.env.VITE_ANOTHERID,
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const database = getDatabase(app);
 
 const ContactForm = () => {
   const [formData, setFormData] = useState({
@@ -9,14 +24,23 @@ const ContactForm = () => {
     email: "",
     message: "",
   });
-
+  const [showPopup, setShowPopup] = useState(false);
+  const [isSubmitted, setIsSubmitted] = useState(false);
   const handleChange = (e) => {
     setFormData({ ...formData, [e.target.name]: e.target.value });
   };
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    console.log("Form Submitted", formData);
+    setIsSubmitted(true);
+    setShowPopup(true);
+
+    // Hide the popup after 3 seconds
+    setTimeout(() => {
+      setShowPopup(false);
+      setIsSubmitted(false);
+      setFormData({ name: "", email: "", message: "" }); // Reset form after popup disappears
+    }, 2000);
   };
 
   return (
@@ -27,60 +51,59 @@ const ContactForm = () => {
           Please fill out the following form and we will get back to you within
           the next 24 hours to help you become even greater at what you do.
         </p>
-        <form onSubmit={handleSubmit} className="contact-form">
-          {/* Name Field */}
-          <div className="form-group">
-            <label htmlFor="name">
-              What's your name? <span>*</span>
-            </label>
-            <input
-              type="text"
-              id="name"
-              name="name"
-              placeholder="Enter your name"
-              value={formData.name}
-              onChange={handleChange}
-              required
-            />
-          </div>
-
-          {/* Email Field */}
-          <div className="form-group">
-            <label htmlFor="email">
-              What is your email address? <span>*</span>
-            </label>
-            <input
-              type="email"
-              id="email"
-              name="email"
-              placeholder="Enter your email"
-              value={formData.email}
-              onChange={handleChange}
-              required
-            />
-          </div>
-
-          {/* Message Field */}
-          <div className="form-group">
-            <label htmlFor="message">
-              Write your message here <span>*</span>
-            </label>
-            <textarea
-              id="message"
-              name="message"
-              placeholder="Enter your message"
-              value={formData.message}
-              onChange={handleChange}
-              required
-            ></textarea>
-          </div>
-        </form>{" "}
-        <div className="submission">
-          {" "}
-          <button type="submit" className="submit-button">
-            Send Message
-          </button>
+        <div className={`popup-message ${showPopup ? "show" : ""}`}>
+          Message sent successfully!
         </div>
+        {!isSubmitted && (
+          <form onSubmit={handleSubmit} className="contact-form">
+            <div className="form-group">
+              <label htmlFor="name">
+                What's your name? <span>*</span>
+              </label>
+              <input
+                type="text"
+                id="name"
+                name="name"
+                placeholder="Enter your name"
+                value={formData.name}
+                onChange={handleChange}
+                required
+              />
+            </div>
+            <div className="form-group">
+              <label htmlFor="email">
+                What is your email address? <span>*</span>
+              </label>
+              <input
+                type="email"
+                id="email"
+                name="email"
+                placeholder="Enter your email"
+                value={formData.email}
+                onChange={handleChange}
+                required
+              />
+            </div>
+            <div className="form-group">
+              <label htmlFor="message">
+                Write your message here <span>*</span>
+              </label>
+              <textarea
+                id="message"
+                name="message"
+                placeholder="Enter your message"
+                value={formData.message}
+                onChange={handleChange}
+                required
+              ></textarea>
+            </div>
+            <div className="submission">
+              <button type="submit" className="submit-button">
+                Send Message
+              </button>
+            </div>
+          </form>
+        )}
       </div>
     </div>
   );
